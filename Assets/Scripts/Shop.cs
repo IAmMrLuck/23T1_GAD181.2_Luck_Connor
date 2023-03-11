b10314486a2377a2464a41904a9efbdc8a7ce229
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2023-03-12 09:04:08 +1100

    building the shop piece by piece
    I'm going to attempt to build the shop mechanic without any assistance from tutorials

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ConaLuk
+{
+    public class Shop : MonoBehaviour
+    {
+        [SerializeField] private Canvas shopcanvas;
+
+        [SerializeField] private Button upgradeOne;
+        [SerializeField] private Button upgradeTwo;
+        [SerializeField] private Button upgradeThree;
+        [SerializeField] private Button upgradeFour;
+
+        private void Start()
+
+        {
+            shopcanvas.enabled = false;
+            upgradeOne.enabled = false;
+        }
+
+        public void ShopOn()
+        {
+            shopcanvas.enabled=true;
+        }
+    }
+}
